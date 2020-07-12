b969b4f7df802177ded278650722643dacf17341
abiha456 <abiha.amir456@gmail.com>
2020-07-12 14:37:59 -0500

    php assignment 2

--- php2.php
+++ php2.php
@@ -16,7 +16,7 @@
     echo "<h2>Pyramid 2</h2>";
 
 
-    for($i=0; $i <= 5; $i++) {
+    for($i=1; $i <= 5; $i++) {
         for($j=5; $j > $i; $j--) {
             echo "&nbsp;&nbsp;&nbsp;";
         }
